9b98f5aefa00b6101d9a41cbcfd6a5f5c83ef339
Nick Rigano <nrigano@Executor.local>
2016-09-20 21:28:53 -0400

    Complete

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -21,48 +21,70 @@ let b = 32
 
 
 
-/*: Question 1
-### 1. Print the result of a greater than or equal to b
-*/
-// write your code here
+//: Question 1
+//### 1. Print the result of a greater than or equal to b
+var questionOne = a >= b
+
+if questionOne {
+    print("\(questionOne)")
+} else {
+    print("false")
+}
 
 
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
-// write your code here
+
+ print("\(a % b == 0)")
+
 
 
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
-// write your code here
+var questionThree = (y * b) <= a
+questionThree = !questionThree
+print("\(questionThree)")
 
 
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
-// write your code here
-
-
+ a >= b
+print("\(a >= b)")
 
 /*: Question 5
 ### 5. Print "true" if a modulo b is equal to zero
 */
-// write your code here
 
+var questionFive = a % b == 0
+
+if questionFive {
+    print("\(questionFive)")
+} /* else {
+    print("false")
+}
+*/
 
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
-// write your code here
+var questionSix = a / b > 0
 
+if questionSix {
+    print("\(questionSix)")
+}
 
 
 /*: Question 7
 ### 7. Print "true" if y divided by x is greater than three, otherwise print false
 */
-// write your code here
+var questionSeven = (Double(y) / x) > 3
+
+if questionSeven {
+    print("\(questionSeven)")
+}
 
 
 
@@ -70,33 +92,75 @@ let b = 32
 /*: Question 8
 ### 8. Print "true" if y is greater than x and a divided by b is greater than 9
 */
-// write your code here
+var questionEight = Double(y) > (x + Double(a)) / Double(b) && a / b > 9
 
+if questionEight {
+    print("\(questionEight)")
+}
 
 
 
 /*: Question 9
 ### 9. Write a function "isGreater" that takes two Int arguments and returns true if the first is greater than the second and false if they're not
 */
-// write your code here
 
-
-
+func isGreater(first: Int, second: Int) -> Bool {
+    let status = first > second
+    if status == true {
+        print("true")
+    } else {
+        print("false")
+    }
+    return status
+}
+/* let status = first > second
+ print("\(status)")
+ return status
+ */
 
 
 
 /*: Question 10
 ### 10. Write a function "isForceWith" that takes a String argument and returns true if the argument is the name of someone with whom the force is strong, and otherwise returns false. People who have the force are Luke, Leia, Anakin, Obi Wan, Yoda, Vader.
 */
-// write your code here
-
+func isForceWith(jedi: String) -> Bool {
+    var theForce: Bool = false
+    if jedi == "Luke" {
+        theForce = true
+    } else if jedi == "Leia" {
+        theForce = true
+    } else if jedi == "Anakin" {
+        theForce = true
+    } else if jedi == "Obi Wan" {
+        theForce = true
+    } else if jedi == "Yoda" {
+        theForce = true
+    } else if jedi == "Vader" {
+        theForce = true
+    }
+    return theForce
+}
 
 
 /*: Question 11
 ### 11. Create a function where the two arguments represent different bank account (one of those bank accounts is yours). What type should these arguments be if we are to then perform some math operations on them? Setup a conditional that will add 10 to your funds and minus 10 from the other funds (bank account) if the other account won't go negative if we were to take 10 dollars from it.
 */
-// write your code here
+//They should be Doubles.  You could also use Floats.
 
+/*
+func transferFunds(myAccount: Double, momsAccount: Double) {
+    if myAccount + 10 > 0 && momsAccount - 10 >0 {
+        myAccount = myAccount + 10
+        momsAccount = momsAccount - 10
+    }
+}
+*/
+func transferTenDollars(myAccount: Int, otherAccount: Int) {
+    if otherAccount >= 10 {
+        myAccount + 10
+        otherAccount - 10
+    }
+}
 
 
 
